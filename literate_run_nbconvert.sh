2da4ede03963c90282e6c39b98f86d5861e600f2
Lyndon White <oxinabox@ucc.asn.au>
2019-09-22 10:18:26 +0100

    remove commented out code

--- literate_run_nbconvert.sh
+++ literate_run_nbconvert.sh
@@ -5,7 +5,3 @@ jupyter nbconvert  --to script "$1" --config literate_config.py --template=liter
 
 outputname=$(basename "$1" .ipynb)
 outputpath=${1%.ipynb}
-
-#postname=$(echo $outputname | tr " " -)
-#mv "$outputpath.md" "../../_posts/$postname.md"
-#mv  "$outputname"_files ../../posts_assets/
